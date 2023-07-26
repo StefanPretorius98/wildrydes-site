eee839229161e51e809c223d4b86a15f3618c804
StefanPretorius98 <88983430+StefanPretorius98@users.noreply.github.com>
2023-07-26 14:42:32 +0200

    Add files via upload

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_ofiHgBuZf', 
+        userPoolClientId: '59a9g5com623eo8c0sn13i2q25',
+        region: 'us-east-1' 
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
